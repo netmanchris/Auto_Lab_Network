1da66863cb1f08b73fa059ff706566053c15ab24
netmanchris <chris_young@me.com>
2016-07-26 13:13:59 -0400

    Added J2 tempalte for generating 5400R configs Added netglobal.yaml for global variables and structure for mobile first branch

---
+++ infraconfigs/gen_configs.py
@@ -0,0 +1,30 @@
+from jinja2 import Environment, FileSystemLoader, Template
+import yaml
+import json
+
+ENV = Environment(loader=FileSystemLoader('./'))
+
+with open("./vars/netglobal.yaml") as inputfile:
+    netglobal =  yaml.load(inputfile)
+
+with open('./vars/mobile_first/devices.yaml') as inputfile:
+    devices = yaml.load(inputfile)
+
+with open('./vars/mobile_first/branch_vars.yaml') as inputfile:
+    branch = yaml.load(inputfile)
+
+for device in devices:
+    if device['type'] == "5406R":
+        template = ENV.get_template("./templates/5406R.j2.")
+        #print (template.render(netglobals=netglobals, device=device))
+        with open("./Configs/"+device['sysname']+"_"+device['manageip']+".cfg", "w") as file:
+            file.write(template.render(netglobal=netglobal, device=device))
+
+
+# Print dictionary generated from yaml
+
+
+# Render template and print generated config to console
+#template = ENV.get_template("Comware5_Template.j2")
+#print (template.render(network_global=network_global, device_values = device_values, site= site))
+
